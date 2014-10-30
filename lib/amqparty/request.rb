1ac360d11ce505fc71543300bcff097dd9b01ed0
Evan Light <elight@optoro.com>
2014-10-30 12:36:44 -0400

    Support for optional async
    Depends on this PR against rack-amqp-client: https://github.com/rack-amqp/rack-amqp-client/pull/1

--- lib/amqparty/request.rb
+++ lib/amqparty/request.rb
@@ -15,6 +15,7 @@ module AMQParty
       path = "#{uri.host}#{uri.path}"
       path = "#{path}?#{uri.query}" if uri.query
       connection_options = options[:amqp_client_options]
+      async              = options[:async]
 
       Rack::AMQP::Client.with_client(connection_options) do |client|
         method_name = http_method.name.split(/::/).last.upcase
@@ -26,30 +27,36 @@ module AMQParty
 
         headers = options[:headers] || {}
         timeout = options[:request_timeout]
+        async   = options[:async]
 
         response = client.request(path, {
             body: body,
             http_method: method_name,
             headers: headers,
-            timeout: timeout
+            timeout: timeout,
+            async: async
           }
         )
 
-        response_code = response.response_code
-        klass = Net::HTTPResponse.send(:response_class, response_code.to_s)
-        http_response = klass.new("1.1", response_code, "Found")
-        response.headers.each_pair do |key, value|
-          http_response.add_field key, value
-        end
-        http_response.body = response.payload
+        unless async
+          response_code = response.response_code
+          klass = Net::HTTPResponse.send(:response_class, response_code.to_s)
+          http_response = klass.new("1.1", response_code, "Found")
+          response.headers.each_pair do |key, value|
+            http_response.add_field key, value
+          end
+          http_response.body = response.payload
 
-        # TODO GIANT HACK
-        http_response.send(:instance_eval, "def body; @body; end")
-        self.last_response = http_response
+          # TODO GIANT HACK
+          http_response.send(:instance_eval, "def body; @body; end")
+          self.last_response = http_response
+        end
       end
 
-      handle_deflation unless http_method == Net::HTTP::Head
-      handle_response(chunked_body, &block)
+      unless options[:async]
+        handle_deflation unless http_method == Net::HTTP::Head
+        handle_response(chunked_body, &block)
+      end
     end
   end
 end
